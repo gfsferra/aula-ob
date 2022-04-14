c018d55818ad45bc48f9c29bd1b61fdbe1bf405f
Gabriel Sferra <83479037+Hyperzinhu@users.noreply.github.com>
2022-04-13 22:15:04 -0300

    Add files via upload

--- aula09/Conta.java
+++ aula09/Conta.java
@@ -5,9 +5,17 @@ import java.io.Console;
 public class Conta{
     private int numero;
     private double saldo;
+    private static int totalContas = 0;
+
+    public Conta(int numero, double saldo){
+        setNumero(numero);
+        setSaldo(saldo);
+        totalContas++;
+    }
 
     public Conta(){
         System.out.println("Sua conta");
+        totalContas++;
     }
 
     private void setNumero(int numero){
@@ -28,11 +36,11 @@ public class Conta{
         }
     }
 
-    private int getNumero(){
+    public int getNumero(){
         return numero;
     }
 
-    private double getSaldo() {
+    public double getSaldo() {
         return saldo;
     }
     
@@ -59,13 +67,16 @@ public class Conta{
     }
 
     public void status(){
-        System.out.println("Conta:" + this.numero);
-        System.out.println("Saldo:" + this.saldo);
+        System.out.println("Conta: " + this.numero);
+        System.out.println("Saldo: " + this.saldo);
     }
 
 
     public static void main(String[] args) {
         Conta conta1 = new Conta();
+        Conta conta2 = new Conta(123, 1000.00);
+        Conta conta3 = new Conta(456, 2000.00);
+        Conta conta4 = new Conta(789, 3000.00);
         conta1.setNumero(1);
         conta1.setSaldo(20000);
         conta1.status();
@@ -75,6 +86,14 @@ public class Conta{
         conta1.credito(777);
         conta1.credito();
         conta1.status();
+        System.out.println("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
+        conta2.status();
+        System.out.println("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
+        conta3.status();
+        System.out.println("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
+        conta4.status();
+        System.out.println("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
+        System.out.println("Total de contas criadas: " + Conta.totalContas);
         }
 
 }
